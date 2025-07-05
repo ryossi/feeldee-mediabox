8ecb881f814425addb868e46a4366c1138de21c5
Ryo Sskai <r.sakai@aifulmf.com>
2025-07-06 06:30:05 +0900

    フレームワークから移植

---
+++ src/Casts/URL.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Feeldee\MediaBox\Casts;
+
+use Feeldee\MediaBox\Models\MediaBox;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class URL implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return MediaBox::url($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return MediaBox::path($value);
+    }
+}
---
+++ src/Facades/ImageText.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Feeldee\MediaBox\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Feeldee\MediaBox\Facades\ImageText
+ *
+ * @method static string resize(string $text, mixed $width = null, mixed $height = null, int $quality = 90)
+ * @method static bool isImageText(?string $text)
+ */
+class ImageText extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return \Feeldee\Framework\Services\ImageTextService::class;
+    }
+}
